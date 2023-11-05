039282976e4569968350993907033edef360fc20
Eliezer Bittencourt <likobittencourt@gmail.com>
2023-11-05 10:11:03 -0300

    adjust: remove comments

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,13 +9,11 @@ const User = require('../models/User');
 router.post('/signup', async (req, res) => {
     const { name, email, password } = req.body;
 
-    // Verificar se o e-mail já está em uso
     const existingUser = await User.findByEmail(email);
     if (existingUser) {
         return res.status(400).json({ error: 'E-mail já está em uso.' });
     }
 
-    // Hash da senha antes de salvar no banco de dados
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
@@ -32,19 +30,16 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    // Verificar se o e-mail está registrado
     const user = await User.findByEmail(email);
     if (!user) {
         return res.status(401).json({ error: 'Credenciais inválidas.' });
     }
 
-    // Verificar se a senha está correta
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
         return res.status(401).json({ error: 'Credenciais inválidas.' });
     }
 
-    // Gerar um token de autenticação
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.json({ message: 'Login bem-sucedido!', token });
