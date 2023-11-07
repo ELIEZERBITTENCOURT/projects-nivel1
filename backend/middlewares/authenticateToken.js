5acdf1c013c253cfabfaf91aa964de62e39c6c83
Eliezer Bittencourt <likobittencourt@gmail.com>
2023-11-07 18:53:37 -0300

    created middleware of authentication

---
+++ backend/middlewares/authenticateToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_SECRET;
+
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ error: 'Acesso não autorizado.' });
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) return res.status(403).json({ error: 'Token inválido.' });
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = authenticateToken;
